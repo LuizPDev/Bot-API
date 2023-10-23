7729bfc7395de6fa09352c58e67b20b8d5cbfac7
LuizPDev <72711812+LuizPDev@users.noreply.github.com>
2023-10-23 17:54:56 -0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,4 +1,3 @@
-// See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
 
 const server = jsonServer.create()
@@ -11,13 +10,12 @@ const server = jsonServer.create()
 // const db = JSON.parse(data);
 // const router = jsonServer.router(db)
 
-// Comment out to allow write operations
 const router = jsonServer.router('db.json')
 
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
-// Add this before server.use(router)
+
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
     '/blog/:resource/:id/show': '/:resource/:id'
@@ -27,5 +25,4 @@ server.listen(3000, () => {
     console.log('JSON Server is running')
 })
 
-// Export the Server API
 module.exports = server
