0a72b86cbd6d4dd7a825c3ffb12b5be7f53f11f3
LuizPDev <72711812+LuizPDev@users.noreply.github.com>
2023-10-23 18:38:08 -0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,4 +1,7 @@
 const jsonServer = require('json-server')
+const jwt = require('jsonwebtoken');
+const express = require('express');
+const app = express();
 
 const server = jsonServer.create()
 
@@ -9,6 +12,34 @@ const server = jsonServer.create()
 // const data = fs.readFileSync(filePath, "utf-8");
 // const db = JSON.parse(data);
 // const router = jsonServer.router(db)
+
+const secretKey = 'sua_chave_secreta';
+
+// Rota de autenticação
+app.post('/login', (req, res) => {
+  // Verifique as credenciais do usuário
+  // Se as credenciais estiverem corretas, gere um token JWT
+  const payload = { user_id: 123, username: 'exampleuser' };
+  const token = jwt.sign(payload, secretKey);
+  res.json({ token });
+});
+
+// Rota protegida que requer autenticação
+app.get('/protegido', (req, res) => {
+  const token = req.headers.authorization;
+  try {
+    const decoded = jwt.verify(token, secretKey);
+    // Token válido, continue o processamento
+    res.json({ mensagem: 'Rota protegida' });
+  } catch (error) {
+    // Token inválido, retorne um erro
+    res.status(401).json({ mensagem: 'Token inválido' });
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Servidor em execução na porta 3000');
+});
 
 const router = jsonServer.router('db.json')
 
